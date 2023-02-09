61a920284e30bf6384289f0c9a3806ba3a647601
Lorenzo Mazzarotto <deinitializer@icloud.com>
2023-02-08 23:25:50 -0300

    Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
